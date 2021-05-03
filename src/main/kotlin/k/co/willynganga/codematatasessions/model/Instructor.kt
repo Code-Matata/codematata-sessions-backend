4ee49fa1b362a6bbee62dd285a3d7b7ad20b3252
willyngash <willynganga024@gmail.com>
2021-05-03 15:00:11 +0300

    Created relevant model classes

---
+++ src/main/kotlin/k/co/willynganga/codematatasessions/model/Instructor.kt
@@ -0,0 +1,17 @@
+package k.co.willynganga.codematatasessions.model
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "instructors")
+data class Instructor(
+    @Id
+    val uuid: String,
+    @Column(unique = true)
+    val username: String,
+    @Column(unique = true)
+    val email: String,
+    val password: String,
+    @OneToMany(mappedBy = "instructor")
+    val sessions: Set<Recordings>
+)
---
+++ src/main/kotlin/k/co/willynganga/codematatasessions/model/Recordings.kt
@@ -0,0 +1,18 @@
+package k.co.willynganga.codematatasessions.model
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "recordings")
+data class Recordings(
+    @Id
+    val id: Long,
+    val title: String,
+    val description: String,
+    val videoUrl: String,
+    val imageUrl: String,
+    val date: String,
+    @ManyToOne
+    @JoinColumn(name = "instructor_uuid")
+    val instructor: Instructor
+)
