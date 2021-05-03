46ea2057736692531882b0fbab25c5461764efba
willyngash <willynganga024@gmail.com>
2021-05-03 13:25:02 +0300

    Initial commit

---
+++ src/main/kotlin/k/co/willynganga/codematatasessions/CodematataSessionsApplication.kt
@@ -0,0 +1,11 @@
+package k.co.willynganga.codematatasessions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodematataSessionsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CodematataSessionsApplication>(*args)
+}
---
+++ src/test/kotlin/k/co/willynganga/codematatasessions/CodematataSessionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package k.co.willynganga.codematatasessions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodematataSessionsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
