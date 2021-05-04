7e900dbe5a843a3ad71c74081f17a0cd1101e443
willyngash <willynganga024@gmail.com>
2021-05-04 13:10:26 +0300

    Made the class open

--- src/main/kotlin/k/co/willynganga/codematatasessions/CodematataSessionsApplication.kt
+++ src/main/kotlin/k/co/willynganga/codematatasessions/CodematataSessionsApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CodematataSessionsApplication
+open class CodematataSessionsApplication
 
 fun main(args: Array<String>) {
 	runApplication<CodematataSessionsApplication>(*args)
