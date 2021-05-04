ad1b48690f3b4c100569f8c80ad25b8e7fe696c8
willyngash <willynganga024@gmail.com>
2021-05-04 16:24:53 +0300

    Wrote some tests for the student repository interface

--- src/test/kotlin/k/co/willynganga/codematatasessions/CodematataSessionsApplicationTests.kt
+++ src/test/kotlin/k/co/willynganga/codematatasessions/CodematataSessionsApplicationTests.kt
@@ -1,13 +1,31 @@
 package k.co.willynganga.codematatasessions
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class CodematataSessionsApplicationTests {
 
+	private val underTest = Calculator()
+
 	@Test
-	fun contextLoads() {
+	fun `it should add two integer numbers`() {
+		// given
+		val a = 20;
+		val b = 30;
+
+		// when
+		val result = underTest.add(a, b)
+
+		//then
+		assertThat(result).isEqualTo(50)
+	}
+
+	class Calculator {
+		fun add(a: Int, b: Int): Int {
+			return a + b
+		}
 	}
 
 }
