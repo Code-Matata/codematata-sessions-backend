6713cd928b3fc8aa0630e455ff4be15d8ef5d2fc
willyngash <willynganga024@gmail.com>
2021-05-15 14:01:16 +0300

    🤓 updated tests

--- src/test/kotlin/k/co/willynganga/codematatasessions/controller/MainControllerTest.kt
+++ src/test/kotlin/k/co/willynganga/codematatasessions/controller/MainControllerTest.kt
@@ -4,6 +4,7 @@ import com.google.gson.Gson
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import k.co.willynganga.codematatasessions.model.Instructor
+import k.co.willynganga.codematatasessions.model.Recording
 import k.co.willynganga.codematatasessions.model.Response
 import k.co.willynganga.codematatasessions.model.Student
 import k.co.willynganga.codematatasessions.service.InstructorService
@@ -347,5 +348,205 @@ internal class MainControllerTest(@Autowired val mockMvc: MockMvc) {
             .andExpect(jsonPath("\$.uuid").value(instructor.uuid))
             .andExpect(jsonPath("\$.email").value(instructor.email))
             .andExpect(jsonPath("\$.username").value(instructor.username))
+    }
+
+    @Test
+    fun `can get all recordings`() {
+        //given
+        val recording = Recording(
+            1,
+            "Spring Boot",
+            "An introduction to spring boot and Kotlin",
+            "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "https://youtu.be/Geq60OVyBPg",
+            "06-05-2021",
+            "Jane Doe"
+        )
+
+        //when
+        every { recordingService.findAllRecordings() } returns listOf(recording)
+
+        //then
+        mockMvc.perform(
+            get("/api/v1/recording/all")
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.[0].id").value(recording.id))
+            .andExpect(jsonPath("\$.[0].title").value(recording.title))
+    }
+
+    @Test
+    fun `can add a recording`() {
+        //given
+        val recording = Recording(
+            1,
+            "Spring Boot",
+            "An introduction to spring boot and Kotlin",
+            "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "https://youtu.be/Geq60OVyBPg",
+            "06-05-2021",
+            "Jane Doe"
+        )
+        val body = Gson().toJson(recording)
+        //when
+        every { recordingService.addRecording(recording) } returns Response(
+            0,
+            STATUS.SUCCESS,
+            "Recording Successfully Saved!"
+        )
+
+        //then
+        mockMvc.perform(
+            post("/api/v1/recording/add")
+                .content(body)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.requestCode").value(0))
+            .andExpect(jsonPath("\$.message").value("Recording Successfully Saved!"))
+    }
+
+    @Test
+    fun `can get recording by id`() {
+        //given
+        val id: Long = 1
+        val recording = Recording(
+            id,
+            "Spring Boot",
+            "An introduction to spring boot and Kotlin",
+            "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "https://youtu.be/Geq60OVyBPg",
+            "06-05-2021",
+            "Jane Doe"
+        )
+
+        //when
+        every { recordingService.findRecordingById(id) } returns recording
+
+        //then
+        mockMvc.perform(
+            get("/api/v1/recording/$id")
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.id").value(recording.id))
+            .andExpect(jsonPath("\$.title").value(recording.title))
+    }
+
+    @Test
+    fun `can get recording by title`() {
+        //given
+        val title = "Spring Boot"
+        val recording = Recording(
+            1,
+            title,
+            "An introduction to spring boot and Kotlin",
+            "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "https://youtu.be/Geq60OVyBPg",
+            "06-05-2021",
+            "Jane Doe"
+        )
+
+        //when
+        every { recordingService.findRecordingByTitle(title) } returns recording
+
+        //then
+        mockMvc.perform(
+            get("/api/v1/recording/by-title")
+                .param("title", title)
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.id").value(recording.id))
+            .andExpect(jsonPath("\$.title").value(recording.title))
+    }
+
+    @Test
+    fun `can get recordings by instructor username`() {
+        //given
+        val username = "Jane Doe"
+        val recording = Recording(
+            1,
+            "Spring Boot",
+            "An introduction to spring boot and Kotlin",
+            "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "https://youtu.be/Geq60OVyBPg",
+            "06-05-2021",
+            username
+        )
+
+        //when
+        every { recordingService.findRecordingByInstructorUsername(username) } returns  listOf(recording)
+
+        //then
+        mockMvc.perform(
+            get("/api/v1/recording/by-instructor")
+                .param("username", username)
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.[0].id").value(recording.id))
+            .andExpect(jsonPath("\$.[0].title").value(recording.title))
+    }
+
+    @Test
+    fun `can get recording by date`() {
+        //given
+        val date = "06-05-2021"
+        val recording = Recording(
+            1,
+            "Spring Boot",
+            "An introduction to spring boot and Kotlin",
+            "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "https://youtu.be/Geq60OVyBPg",
+            date,
+            "test"
+        )
+
+        //when
+        every { recordingService.findRecordingByDate(date) } returns listOf(recording)
+
+        //then
+        mockMvc.perform(
+            get("/api/v1/recording/by-date")
+                .param("date", date)
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.[0].id").value(recording.id))
+            .andExpect(jsonPath("\$.[0].title").value(recording.title))
+    }
+
+    @Test
+    fun `can get recording by title and date`() {
+        //given
+        val date = "06-05-2021"
+        val title = "Spring Boot"
+        val recording = Recording(
+            1,
+            title,
+            "An introduction to spring boot and Kotlin",
+            "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "https://youtu.be/Geq60OVyBPg",
+            date,
+            "test"
+        )
+
+        //when
+        every { recordingService.findRecordingByTitleAndDate(title, date) } returns listOf(recording)
+
+        //then
+        mockMvc.perform(
+            get("/api/v1/recording/by-title-and-date")
+                .param("title", title)
+                .param("date", date)
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.[0].id").value(recording.id))
+            .andExpect(jsonPath("\$.[0].title").value(recording.title))
     }
 }
