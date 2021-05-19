1182cea51b1480ab8f40841c82cb28d01c64c420
willyngash <willynganga024@gmail.com>
2021-05-19 16:52:10 +0300

    🤞 add more configuration

--- src/main/kotlin/k/co/willynganga/codematatasessions/config/WebSecurityConfig.kt
+++ src/main/kotlin/k/co/willynganga/codematatasessions/config/WebSecurityConfig.kt
@@ -1,43 +1,61 @@
 package k.co.willynganga.codematatasessions.config
 
-import k.co.willynganga.codematatasessions.security.CustomOAuth2User
+import k.co.willynganga.codematatasessions.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository
+import k.co.willynganga.codematatasessions.security.oauth2.OAuth2AuthenticationSuccessHandler
 import k.co.willynganga.codematatasessions.service.CustomOAuth2UserService
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 
 @Configuration
 @EnableWebSecurity
-open class WebSecurityConfig constructor(@Autowired private val customOAuth2UserService: CustomOAuth2UserService) : WebSecurityConfigurerAdapter() {
+@EnableGlobalMethodSecurity(
+    securedEnabled = true,
+    jsr250Enabled = true,
+    prePostEnabled = true
+)
+open class WebSecurityConfig constructor(
+    private val customOAuth2UserService: CustomOAuth2UserService,
+    private val oAuth2AuthenticationSuccessHandler: OAuth2AuthenticationSuccessHandler
+) : WebSecurityConfigurerAdapter() {
 
     private val logger = LoggerFactory.getLogger(WebSecurityConfig::class.java)
 
+    @Bean
+    open fun cookieAuthorizationRequestRepository(): HttpCookieOAuth2AuthorizationRequestRepository {
+        return HttpCookieOAuth2AuthorizationRequestRepository()
+    }
+
     override fun configure(http: HttpSecurity?) {
         http!!
+            .cors()
+                .and()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+            .csrf()
+                .disable()
             .authorizeRequests()
-            .antMatchers("/login", "/oauth/**", "/oauth2/**").permitAll()
+            .antMatchers("/oauth/**", "/oauth2/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin()
-            .disable()
+                .disable()
             .httpBasic()
-            .disable()
+                .disable()
             .oauth2Login()
-            .authorizationEndpoint()
-            .baseUri("/oauth2/authorize")
-            .and()
-            .userInfoEndpoint()
-            .userService(customOAuth2UserService)
-            .and()
-            .successHandler { request, response, authentication ->
-                val oauthUser = authentication!!.principal as CustomOAuth2User
-                logger.info(oauthUser.name)
-                logger.info(oauthUser.attributes.toString())
-                logger.info(oauthUser.authorities.toString())
-                response.sendRedirect("http://localhost:3000/")
-            }
+                .authorizationEndpoint()
+                    .baseUri("/oauth2/authorize")
+                    .authorizationRequestRepository(cookieAuthorizationRequestRepository())
+                    .and()
+                .userInfoEndpoint()
+                    .userService(customOAuth2UserService)
+                    .and()
+                .successHandler(oAuth2AuthenticationSuccessHandler)
     }
 }
