17c6b8ab7d67308406f958d73a9593df42ffbb87
willyngash <willynganga024@gmail.com>
2021-05-19 16:53:20 +0300

    🤞 provide constants and enums

---
+++ src/main/kotlin/k/co/willynganga/codematatasessions/other/Constants.kt
@@ -0,0 +1,10 @@
+package k.co.willynganga.codematatasessions.other
+
+class Constants {
+
+    companion object {
+        const val OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request"
+        const val REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri"
+        const val cookieExpireSeconds = 180
+    }
+}
---
+++ src/main/kotlin/k/co/willynganga/codematatasessions/other/PROVIDER.kt
@@ -0,0 +1,5 @@
+package k.co.willynganga.codematatasessions.other
+
+enum class PROVIDER {
+    GOOGLE, GITHUB
+}
