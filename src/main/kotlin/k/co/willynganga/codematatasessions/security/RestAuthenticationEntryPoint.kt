78195357822f66abb009d531844636aa8659b7ce
willyngash <willynganga024@gmail.com>
2021-05-20 20:39:25 +0300

    🤩 handle unauthorized requests

---
+++ src/main/kotlin/k/co/willynganga/codematatasessions/security/RestAuthenticationEntryPoint.kt
@@ -0,0 +1,27 @@
+package k.co.willynganga.codematatasessions.security
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class RestAuthenticationEntryPoint : AuthenticationEntryPoint {
+
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(RestAuthenticationEntryPoint::class.java)
+    }
+
+    override fun commence(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        exception: AuthenticationException?
+    ) {
+        logger.error("Responding with unauthorized error.  Message - ${exception?.localizedMessage}")
+        response?.sendError(
+            HttpServletResponse.SC_UNAUTHORIZED,
+            exception?.localizedMessage
+        )
+    }
+}
