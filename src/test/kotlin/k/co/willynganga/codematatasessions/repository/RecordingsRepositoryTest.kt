cf0d1b713abd6e51a10dae824894f6eb0d43c9ed
willyngash <willynganga024@gmail.com>
2021-05-24 21:26:27 +0300

    Code clean up

--- src/test/kotlin/k/co/willynganga/codematatasessions/repository/RecordingsRepositoryTest.kt
+++ src/test/kotlin/k/co/willynganga/codematatasessions/repository/RecordingsRepositoryTest.kt
@@ -1,6 +1,5 @@
 package k.co.willynganga.codematatasessions.repository
 
-import k.co.willynganga.codematatasessions.model.Instructor
 import k.co.willynganga.codematatasessions.model.Recording
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -11,8 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
 @DataJpaTest
 open class RecordingsRepositoryTest @Autowired constructor(
-    private val underTest: RecordingsRepository,
-    private val instructorRepository: InstructorRepository
+    private val underTest: RecordingsRepository
 ) {
 
     @BeforeEach
@@ -25,12 +23,6 @@ open class RecordingsRepositoryTest @Autowired constructor(
     fun `it should find recording with valid title`() {
         //given
         val title = "Spring Boot"
-        val instructor = Instructor(
-            "849fd6dd-5ae1-47f0-9640-b3cb34a9392a",
-            "test",
-            "test@test.com",
-            "password"
-        )
         val recording = Recording(
             title,
             "An introduction to spring boot and Kotlin",
@@ -39,7 +31,6 @@ open class RecordingsRepositoryTest @Autowired constructor(
             "06-05-2021",
             "test"
         )
-        instructorRepository.save(instructor)
         underTest.save(recording)
         //when
         val exists = underTest.findByTitle(title)
