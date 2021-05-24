ab3f0f8a90781edfb4dfae33ac5045043c7f1c9d
willyngash <willynganga024@gmail.com>
2021-05-24 21:25:51 +0300

    Add more methods

--- src/main/kotlin/k/co/willynganga/codematatasessions/service/OAuthUserService.kt
+++ src/main/kotlin/k/co/willynganga/codematatasessions/service/OAuthUserService.kt
@@ -1,12 +1,14 @@
 package k.co.willynganga.codematatasessions.service
 
 import k.co.willynganga.codematatasessions.model.OAuthUser
+import k.co.willynganga.codematatasessions.model.Response
 import k.co.willynganga.codematatasessions.repository.OAuth2UserRepository
+import k.co.willynganga.codematatasessions.util.STATUS
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-open class OAuthUserService constructor(@Autowired private val repository: OAuth2UserRepository) {
+class OAuthUserService constructor(@Autowired private val repository: OAuth2UserRepository) {
 
     //find all
     fun findAllUsers(): List<OAuthUser> = repository.findAll()
@@ -15,6 +17,12 @@ open class OAuthUserService constructor(@Autowired private val repository: OAuth
     fun findByEmail(email: String): OAuthUser? = repository.findByEmail(email)
 
     //add user
-    fun addUser(oAuthUser: OAuthUser) = repository.saveAndFlush(oAuthUser)
+    fun addUser(oAuthUser: OAuthUser): Response {
+        repository.saveAndFlush(oAuthUser)
+        return Response(0, STATUS.SUCCESS, "User added successfully!")
+    }
+
+    //delete user
+    fun deleteUserByEmail(email: String) = repository.deleteByEmail(email)
 
 }
