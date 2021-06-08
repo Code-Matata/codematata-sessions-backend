182a6841bb7352bad43d05bedfe3875d62c9d76d
willyngash <willynganga024@gmail.com>
2021-06-08 18:47:01 +0300

    Use paging

--- src/main/kotlin/k/co/willynganga/codematatasessions/service/EventService.kt
+++ src/main/kotlin/k/co/willynganga/codematatasessions/service/EventService.kt
@@ -1,15 +1,24 @@
 package k.co.willynganga.codematatasessions.service
 
 import k.co.willynganga.codematatasessions.model.Event
+import k.co.willynganga.codematatasessions.model.EventsResponse
 import k.co.willynganga.codematatasessions.model.Response
 import k.co.willynganga.codematatasessions.repository.EventsRepository
 import k.co.willynganga.codematatasessions.util.STATUS
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
 class EventService(val eventsRepository: EventsRepository, val imageService: ImageService) {
 
-    fun getAllEvents(): List<Event> = eventsRepository.findAll()
+    fun getAllEvents(pageable: Pageable): EventsResponse {
+        val page = eventsRepository.findAll(pageable)
+        return EventsResponse(
+            page.totalPages,
+            page.number,
+            page.content
+        )
+    }
 
     fun getEvent(id: Long): Event? = eventsRepository.findById(id).get()
 
