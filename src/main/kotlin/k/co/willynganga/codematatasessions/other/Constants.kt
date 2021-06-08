deed29f061e04f5c3372d922d5b155d0e1ad0523
willyngash <willynganga024@gmail.com>
2021-06-08 08:05:37 +0300

    Add constant

--- src/main/kotlin/k/co/willynganga/codematatasessions/other/Constants.kt
+++ src/main/kotlin/k/co/willynganga/codematatasessions/other/Constants.kt
@@ -7,5 +7,6 @@ class Constants {
         const val REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri"
         const val cookieExpireSeconds = 180
         const val MAX_AGE_SECS: Long = 3600
+        const val IMAGE_BASE_URL = "https://code-matata.herokuapp.com/api/v1/images/"
     }
 }
