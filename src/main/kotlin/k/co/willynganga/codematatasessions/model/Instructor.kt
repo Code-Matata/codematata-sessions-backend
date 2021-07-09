880978f0490c5a914cd0cc59661843e58539397e
willyngash <willynganga024@gmail.com>
2021-07-09 10:34:08 +0300

    Add Instructor data model

---
+++ src/main/kotlin/k/co/willynganga/codematatasessions/model/Instructor.kt
@@ -0,0 +1,33 @@
+package k.co.willynganga.codematatasessions.model
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import javax.persistence.*
+
+@Entity(name = "instructors")
+data class Instructor(
+    @Column(unique = true)
+    val username: String,
+    val name: String,
+    @Column(unique = true)
+    val email: String,
+    val bioInfo: String,
+    val github: String,
+    val twitter: String,
+    val instagram: String,
+    val phone: String,
+    @OneToMany(
+        mappedBy = "instructor",
+        cascade = [CascadeType.ALL],
+        fetch = FetchType.LAZY
+    )
+    @JsonIgnore
+    val recordings: List<Recording> = emptyList(),
+    @OneToMany(
+        mappedBy = "instructor"
+    )
+    @JsonIgnore
+    val events: List<Event> = emptyList(),
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private val id: Long = 0
+)
