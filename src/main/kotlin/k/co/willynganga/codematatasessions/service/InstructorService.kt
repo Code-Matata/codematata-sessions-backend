04c168e24c022c618632c1146594976fb3631437
willyngash <willynganga024@gmail.com>
2021-07-09 10:34:30 +0300

    Add Instructor service

---
+++ src/main/kotlin/k/co/willynganga/codematatasessions/service/InstructorService.kt
@@ -0,0 +1,40 @@
+package k.co.willynganga.codematatasessions.service
+
+import k.co.willynganga.codematatasessions.model.Instructor
+import k.co.willynganga.codematatasessions.model.InstructorsResponse
+import k.co.willynganga.codematatasessions.model.Response
+import k.co.willynganga.codematatasessions.repository.InstructorRepository
+import k.co.willynganga.codematatasessions.util.STATUS
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+
+@Service
+class InstructorService(private val repository: InstructorRepository) {
+
+    fun findByUsername(username: String): Instructor? {
+        return repository.findByUsername(username)
+    }
+
+    fun getAllInstructors(pageable: Pageable): InstructorsResponse {
+        val page = repository.findAll(pageable)
+        return InstructorsResponse(
+            page.totalPages,
+            page.number,
+            page.content
+        )
+    }
+
+    fun addInstructor(instructor: Instructor): Response {
+        repository.saveAndFlush(instructor)
+        return Response(0, STATUS.SUCCESS, "Instructor saved successfully!")
+    }
+
+    fun deleteInstructor(username: String): Response {
+        val instructor = repository.findByUsername(username)
+        instructor?.let {
+            repository.delete(it)
+            return Response(0, STATUS.SUCCESS, "Instructor deleted successfully!")
+        }
+        return Response(1, STATUS.FAIL, "No instructor matches username $username!")
+    }
+}
