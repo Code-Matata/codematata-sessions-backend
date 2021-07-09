085fd721b61d52aa8043d9134ed590262d6cc058
willyngash <willynganga024@gmail.com>
2021-07-09 10:36:22 +0300

    Update test

--- src/test/kotlin/k/co/willynganga/codematatasessions/repository/RecordingsRepositoryTest.kt
+++ src/test/kotlin/k/co/willynganga/codematatasessions/repository/RecordingsRepositoryTest.kt
@@ -1,5 +1,6 @@
 package k.co.willynganga.codematatasessions.repository
 
+import k.co.willynganga.codematatasessions.model.Instructor
 import k.co.willynganga.codematatasessions.model.Recording
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -11,11 +12,13 @@ import org.springframework.data.domain.PageRequest
 
 @DataJpaTest
 open class RecordingsRepositoryTest @Autowired constructor(
-    private val underTest: RecordingsRepository
+    private val underTest: RecordingsRepository,
+    private val instructorRepository: InstructorRepository
 ) {
 
     @BeforeEach
     internal fun setUp() {
+        instructorRepository.deleteAll()
         underTest.deleteAll()
         underTest.flush()
     }
@@ -24,12 +27,24 @@ open class RecordingsRepositoryTest @Autowired constructor(
     fun `it should find recording with valid title`() {
         //given
         val title = "Spring Boot"
+        val instructor = Instructor(
+            "jane-doe",
+            "Jane Doe",
+            "janedo@test.com",
+            "A software engineer at Google!",
+            "github.com/jane-doe",
+            "@janedoe99",
+            "janedoe99",
+            "+1 999 231 232"
+        )
+        instructorRepository.save(instructor)
         val recording = Recording(
             title,
             "An introduction to spring boot and Kotlin",
             "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "github.com/janedoe/spring-boot.git",
             "06-05-2021",
-            "test"
+            instructor = instructor
         )
         underTest.save(recording)
         //when
@@ -43,12 +58,24 @@ open class RecordingsRepositoryTest @Autowired constructor(
     fun `it should not find recording with invalid title`() {
         //given
         val title = "Spring Boot"
+        val instructor = Instructor(
+            "jane-doe",
+            "Jane Doe",
+            "janedo@test.com",
+            "A software engineer at Google!",
+            "github.com/jane-doe",
+            "@janedoe99",
+            "janedoe99",
+            "+1 999 231 232"
+        )
+        instructorRepository.save(instructor)
         val recording = Recording(
             title,
             "An introduction to spring boot and Kotlin",
             "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "github.com/janedoe/spring-boot.git",
             "06-05-2021",
-            "test"
+            instructor = instructor
         )
         underTest.save(recording)
         //when
@@ -59,37 +86,27 @@ open class RecordingsRepositoryTest @Autowired constructor(
     }
 
     @Test
-    fun `find recording using instructor username`() {
-        //given
-        val username = "test"
-        val recording = Recording(
-            "Spring Boot",
-            "An introduction to spring boot and Kotlin",
-            "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
-            "06-05-2021",
-            username
-        )
-        val page = PageRequest.of(0, 12)
-
-        //when
-        underTest.saveAndFlush(recording)
-
-        //then
-        val exists = underTest.findByInstructor(page ,username)
-
-        assertThat(exists.content).isNotNull
-    }
-
-    @Test
     fun `it should find recordings by date`() {
         //given
         val date = "06-05-2021"
+        val instructor = Instructor(
+            "jane-doe",
+            "Jane Doe",
+            "janedo@test.com",
+            "A software engineer at Google!",
+            "github.com/jane-doe",
+            "@janedoe99",
+            "janedoe99",
+            "+1 999 231 232"
+        )
+        instructorRepository.save(instructor)
         val recording = Recording(
             "Spring Boot",
             "An introduction to spring boot and Kotlin",
             "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "github.com/janedoe/spring-boot.git",
             date,
-            "test"
+            instructor = instructor
         )
         val page = PageRequest.of(0, 12)
         //when
@@ -105,12 +122,24 @@ open class RecordingsRepositoryTest @Autowired constructor(
     fun `it should not find recordings with invalid date`() {
         //given
         val date = "12-12-2020"
+        val instructor = Instructor(
+            "jane-doe",
+            "Jane Doe",
+            "janedo@test.com",
+            "A software engineer at Google!",
+            "github.com/jane-doe",
+            "@janedoe99",
+            "janedoe99",
+            "+1 999 231 232"
+        )
+        instructorRepository.save(instructor)
         val recording = Recording(
             "Spring Boot",
             "An introduction to spring boot and Kotlin",
             "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "github.com/janedoe/spring-boot.git",
             "2021-05-06",
-            "test"
+            instructor = instructor
         )
         val page = PageRequest.of(0, 12)
         //when
@@ -127,12 +156,24 @@ open class RecordingsRepositoryTest @Autowired constructor(
         //given
         val date = "06-05-2021"
         val title = "Spring Boot"
+        val instructor = Instructor(
+            "jane-doe",
+            "Jane Doe",
+            "janedo@test.com",
+            "A software engineer at Google!",
+            "github.com/jane-doe",
+            "@janedoe99",
+            "janedoe99",
+            "+1 999 231 232"
+        )
+        instructorRepository.save(instructor)
         val recording = Recording(
             title,
             "An introduction to spring boot and Kotlin",
             "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "github.com/janedoe/spring-boot.git",
             date,
-            "test"
+            instructor = instructor
         )
         val page = PageRequest.of(0, 12)
         //when
@@ -149,12 +190,24 @@ open class RecordingsRepositoryTest @Autowired constructor(
         //given
         val date = "06-05-20211"
         val title = "spring boot"
+        val instructor = Instructor(
+            "jane-doe",
+            "Jane Doe",
+            "janedo@test.com",
+            "A software engineer at Google!",
+            "github.com/jane-doe",
+            "@janedoe99",
+            "janedoe99",
+            "+1 999 231 232"
+        )
+        instructorRepository.save(instructor)
         val recording = Recording(
             "Spring Boot",
             "An introduction to spring boot and Kotlin",
             "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Geq60OVyBPg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+            "github.com/janedoe/spring-boot.git",
             "06-05-2021",
-            "test"
+            instructor = instructor
         )
         val page = PageRequest.of(0, 12)
         //when
