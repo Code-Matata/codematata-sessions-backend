fd858412452c8dea57a0a49adfac3a8cf256df7d
willyngash <willynganga024@gmail.com>
2021-07-13 12:02:41 +0300

    Create event dto

---
+++ src/main/kotlin/k/co/willynganga/codematatasessions/model/EventDto.kt
@@ -0,0 +1,16 @@
+package k.co.willynganga.codematatasessions.model
+
+import k.co.willynganga.codematatasessions.extensions.monthShortForm
+
+class EventDto(event: Event) {
+    val id = event.id
+    val title = event.title
+    val description = event.description
+    val startTime = event.startTime
+    val endTime = event.endTime
+    val meetUrl = event.meetUrl
+    val prerequisites = event.prerequisites
+    val date = Date(event.startTime.dayOfMonth.toString(), event.startTime.monthShortForm())
+    val imageUrl = event.imageUrl
+    val instructor = event.instructor
+}
